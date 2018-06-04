9dfc8c0f17290c48d71528ca51dee3e754386cc6
cthill <7596236+cthill@users.noreply.github.com>
2018-06-04 01:13:19 -0700

    fix cradle spacing

--- server/cradle.sh
+++ server/cradle.sh
@@ -5,7 +5,7 @@ do
     echo "Cradle: non-zero exit code..."
     while [[ $(netstat --inet -natp | grep ":3104\|:3105" | grep "TIME_WAIT") ]]; do
         echo "Cradle: waiting for socket close..."
-	sleep 5
-    done	
+        sleep 5
+    done
     echo "Cradle: restarting server..."
 done
